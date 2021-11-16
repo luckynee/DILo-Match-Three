3bc37552d5e6f4cec2564e0c67cf39390512619c
luckynee <janspurba64@gmail.com>
2021-11-17 00:17:18 +0700

    DILo-Match-Three

---
+++ Assets/Scripts/UIGameOver.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIGameOver : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            UnityEngine.SceneManagement.SceneManager.LoadScene(UnityEngine.SceneManagement.SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+
+    public void Show()
+    {
+        gameObject.SetActive(true);
+    }
+
+    public void Hide()
+    {
+        gameObject.SetActive(false);
+    }
+}
